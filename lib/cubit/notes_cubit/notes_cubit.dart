baa78b4a265dae7eb9ca81b398c11f1b30e12ab3
ghazal mhnna <ghazalmhnna13@gmail.com>
2025-09-15 16:46:52 +0300

    create fetch notes method

--- lib/cubit/notes_cubit/notes_cubit.dart
+++ lib/cubit/notes_cubit/notes_cubit.dart
@@ -1,9 +1,18 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:music_notes_player_app_setup/const/notes_const.dart';
 import 'package:music_notes_player_app_setup/cubit/notes_cubit/notes_state.dart';
+import 'package:music_notes_player_app_setup/models/note_model.dart';
 
-class NotesCubit extends Cubit<NotesState>{
+class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
-  void g (){
-    
+  void fetchAllNotes() {
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+    List <NoteModel> notesList = notesBox.values.toList();
+      emit(NotesSuccess(notesList));
+    } catch (e) {
+      emit(NotesFailure(e.toString()));
+    }
   }
 }
